5c186da8c6d72fc0feaaaf8f20c3c675cf69674a
Sajedul-Islam <sajedulislam.cse@gmail.com>
2022-05-08 08:37:58 +0600

    process env variabe setup for firebase

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth';
 
 //---------firebase initialization and configuration----------
 const firebaseConfig = {
-    apiKey: "AIzaSyDaCGR-VqhLhhcIqDt5KM14SctUMa4J7o4",
-    authDomain: "a11-authentication.firebaseapp.com",
-    projectId: "a11-authentication",
-    storageBucket: "a11-authentication.appspot.com",
-    messagingSenderId: "572824596335",
-    appId: "1:572824596335:web:71b38efcd233386826429a"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
